5c2d0e43ee14b09849caa4059637457b1a936536
hanen2006 <hhcine@yahoo.fr>
2023-10-06 17:07:19 +0200

    remplacement avec des nouvelles fichiers

--- api.py
+++ api.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import flask
 from flask import Flask, jsonify, request, render_template
 import joblib
@@ -11,30 +8,23 @@ import pickle
 import pandas as pd
 import shap
 import json
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = False
-
-
-# In[ ]:
-
+import warnings
 
 # Suppression warnings
-import warnings
 warnings.filterwarnings('ignore')
-#Chargement du tableau et du modèle
 
-df = pd.read_csv("data/X_sample.csv")
-print(df.head())
+# Initialiser l'application Flask
 app = Flask(__name__)
+app.config["DEBUG"] = False
+
+# Charger les données et le modèle
+df = pd.read_csv("data/X_sample.csv")
+df.set_index('SK_ID_CURR', inplace=True)
+print(df.head())
 LGBMClassifier = pickle.load(open("modele/LGBMClassifier.pkl", "rb"))
-
-
 df.drop(columns='TARGET', inplace=True)
 num_client = df.index.unique()
-
-
-
+print(num_client[:10])
 @app.route('/')
 def home():
     return "api"
@@ -42,19 +32,17 @@ def home():
 @app.route('/predict')
 def predict():
     """
-
     Returns
     liste des clients dans le fichier
-
     """
-    return jsonify({"model": "'LGBMClassifier",
-                    "list_client_id" : list(num_client.astype(str))})
-
+    return jsonify({
+        "model": "LGBMClassifier",
+        "list_client_id" : list(num_client.astype(str))
+    })
 
 @app.route('/predict/<int:sk_id>',methods=['GET'])
 def predict_get(sk_id):
     """
-
     Parameters
     ----------
     sk_id : numero de client
@@ -63,9 +51,7 @@ def predict_get(sk_id):
     -------
     prediction  0 pour paiement OK
                 1 pour defaut de paiement
-
     """
-
     if sk_id in num_client:
         predict = LGBMClassifier.predict(df.loc[sk_id].values.reshape(1,-1))
         predict_proba = LGBMClassifier.predict_proba(df.loc[sk_id].values.reshape(1,-1))
@@ -73,43 +59,36 @@ def predict_get(sk_id):
         predict_proba_1 = str(predict_proba[0][1])
     else:
         predict = predict_proba_0 = predict_proba_1 = "client inconnu"
-    return jsonify({ 'retour_prediction' : str(predict), 'predict_proba_0': predict_proba_0,
-                     'predict_proba_1': predict_proba_1 })
+    return jsonify({
+        'retour_prediction' : str(predict),
+        'predict_proba_0': predict_proba_0,
+        'predict_proba_1': predict_proba_1
+    })
+# ... (reste du code)
+
+print(num_client[:10])  # printez les 10 premiers ID pour vérifier
 
 @app.route('/details/<int:sk_id>')
 def client_details(sk_id):
-    """
-    Parameters
-    ----------
-    sk_id : numero de client
-
-    Returns
-    -------
-    Détails des valeurs pour le client spécifié et moyennes de toutes les variables pour tous les clients.
-    """
+    print(f"Received sk_id: {sk_id}")  # Affichez le sk_id reçu
+    print(f"Type of sk_id: {type(sk_id)}")  # Affichez le type de sk_id
+    print(f"First 10 clients in num_client: {num_client[:10]}")  # Affichez les 10 premiers clients
+    print(f"Type of first client in num_client: {type(num_client[0])}")  # Affichez le type du premier client
 
     if sk_id in num_client:
-        # Récupérer les détails du client
         client_data = df.loc[sk_id].to_dict()
-
-        # Récupérer les moyennes pour toutes les variables
         mean_values = df.mean().to_dict()
-
         return jsonify({
             "client_data": client_data,
             "mean_values": mean_values
         })
-
     else:
+        print(f"sk_id {sk_id} not found in num_client")
         return jsonify({"error": "client inconnu"}), 404
 
 
 if __name__ == '__main__':
     app.run()
-
-
-# In[ ]:
-
 
 
 
---
+++ test_api (5).py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+import pytest
+
+from Api import app  
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "api"
+
+def test_predict(client):
+    response = client.get('/predict')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert 'model' in json_data
+    assert json_data['model'] == 'LGBMClassifier'
+    assert 'list_client_id' in json_data
+
+def test_predict_valid_sk_id(client):
+    response = client.get('/predict/146052')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert 'retour_prediction' in json_data
+
+def test_predict_invalid_sk_id(client):
+    response = client.get('/predict/999999999')
+    assert response.status_code == 200  # À ajuster si vous changez le code pour retourner une autre réponse.
+    json_data = response.get_json()
+    assert 'retour_prediction' in json_data
+    assert json_data['retour_prediction'] == 'client inconnu'
+
+def test_details_valid_sk_id(client):
+    response = client.get('/details/146052')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert 'client_data' in json_data
+    assert 'mean_values' in json_data
+
+def test_details_invalid_sk_id(client):
+    response = client.get('/details/999999999')
+    assert response.status_code == 404
+
+
+
+# In[ ]:
+
+
+
+
